e101963d0b96b5434a53f440127dbb260bc58733
concunmun98 <mstaion508@gmail.com>
2020-07-08 10:00:29 +0700

    new test

---
+++ Phan Anh Duc/Week-1  Web_base/Day 3-JavaScripts/information table.js
@@ -0,0 +1,10 @@
+function insochan(){
+	var a = [0,1,2,3,4,5,6,7,8,9];
+	var i = 0;
+	var Sum = a[i] + i;
+	for(i < a.length; i++){                    
+                	document.write(Sum);
+            }
+        }
+ insochan();
+        
