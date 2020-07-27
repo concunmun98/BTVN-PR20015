9c8716970f129e00b555cd4a5522fa700248351d
concunmun98 <mstaion508@gmail.com>
2020-07-27 10:36:03 +0700

    Ruby_ST

--- helloworld.rb
+++ helloworld.rb
@@ -1,4 +1,9 @@
-puts "\t\t\t\t   HELLO WORLD"
-puts "Fullname \t\t Birthday \t\t Address \t\tPhone"
-puts "Le Van Huy \t\t 04/10/1995 \t\t Vinh Phuc \t\t0969088196"
-
+puts "============================================================================================================"
+print "Hello Boss"
+print "\n"
+print "May I help you ?"
+print "\n"
+print "Please . . ."
+print "\n"
+print " Phan Anh Duc \t\t 22/03/1998 \t\t Hanoi \n"
+puts "============================================================================================================"
