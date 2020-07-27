2bc18af5c69c78074292d8aedb5f78fc687e8430
concunmun98 <mstaion508@gmail.com>
2020-07-27 10:23:59 +0700

    Ruby_Chris

--- SQL-BT/C.sql
+++
@@ -1,6 +0,0 @@
-select distinct title 
-from book inner join borrow_detail on book.book_id=borrow_detail.book_id inner join borrow on borrow_detail.borrow_id=borrow.borrow_id 
-where reader_id=(select reader_id 
-	from borrow_detail inner join borrow on borrow_detail.borrow_id=borrow.borrow_id
-							group by reader_id   
-							having count(distinct book_id)>=10)
